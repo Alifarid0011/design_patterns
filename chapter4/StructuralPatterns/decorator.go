c016930685078327eb1674c64b3c14e4d8f85731
a.farid <alifarid0011@gmail.com>
2024-09-12 10:19:14 +0330

    add decorator design pattern

--- chapter4/StructuralPatterns/decorator.go
+++ chapter4/StructuralPatterns/decorator.go
@@ -2,18 +2,18 @@ package main
 
 import (
 	"errors"
+	"fmt"
 )
 
 type IngredientAdd interface {
 	AddIngredient() (string, error)
 }
-
 type PizzaDecorator struct {
 	Ingredient IngredientAdd
 }
 
 func (p *PizzaDecorator) AddIngredient() (string, error) {
-	return "", errors.New("not implemented yet")
+	return "Pizza with the following ingredients:", nil
 }
 
 type Meat struct {
@@ -21,7 +21,14 @@ type Meat struct {
 }
 
 func (m *Meat) AddIngredient() (string, error) {
-	return "", errors.New("not implemented yet")
+	if m.Ingredient == nil {
+		return "", errors.New("An IngredientAdd is needed in the Ingredient field of the Meat")
+	}
+	s, err := m.Ingredient.AddIngredient()
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%s %s,", s, "meat"), nil
 }
 
 type Onion struct {
@@ -29,5 +36,12 @@ type Onion struct {
 }
 
 func (o *Onion) AddIngredient() (string, error) {
-	return "", errors.New("not implemented yet")
+	if o.Ingredient == nil {
+		return "", errors.New("an IngredientAdd is needed in the Ingredient field of the Onion")
+	}
+	s, err := o.Ingredient.AddIngredient()
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%s %s,", s, "onion"), nil
 }
